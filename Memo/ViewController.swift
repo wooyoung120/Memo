8c96d6ff842ca3b74b35223f9cb99feee46749bf
wooyoung <kwy120@icloud.com>
2022-02-26 17:42:28 +0900

    Initial Commit

---
+++ Memo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Memo
+//
+//  Created by Woo Young Kim on 2022/02/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
