9a1ed6f73f6e849f306815ec97a64ef8fefc22d8
kwy120@gmail.com <kwy120@gmail.com>
2022-03-04 16:34:43 +0900

    커스텀 클래스 생성 및 객체 연결

---
+++ Memo/MemoCell.swift
@@ -0,0 +1,23 @@
+//
+//  MemoCell.swift
+//  Memo
+//
+//  Created by wooyoung on 2022/03/04.
+//
+
+import UIKit
+
+class MemoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
