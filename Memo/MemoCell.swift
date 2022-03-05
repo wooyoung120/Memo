24d9a9bb36558b6a4aa67d199c5df3ccee37fc1e
wooyoung <kwy120@icloud.com>
2022-03-05 15:20:38 +0900

    MemoCell 클래스 구현

--- Memo/MemoCell.swift
+++ Memo/MemoCell.swift
@@ -8,16 +8,8 @@
 import UIKit
 
 class MemoCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    @IBOutlet weak var subject: UILabel!    // 메모 제목
+    @IBOutlet weak var contents: UILabel!   // 메모 내용
+    @IBOutlet weak var regdate: UILabel!    // 등록 일자
+    @IBOutlet weak var img: UIImageView!    // 이미지
 }
